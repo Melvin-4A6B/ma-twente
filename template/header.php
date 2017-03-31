b0188c451ef3d8b7758c66d127beb39783c65086
Melvin-4A6B <444277@edu.rocmn.nl>
2017-03-31 15:49:54 +0200

    begonnen met de login pagina

--- template/header.php
+++ template/header.php
@@ -1,3 +1,4 @@
+<?php include 'core/loginModal.php'; ?>
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -5,5 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>MA-Twente</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
 </head>
 <body>
